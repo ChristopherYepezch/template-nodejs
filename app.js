bb89d3d95cc0eed7ca85537e6858f8dd34bdeb7d
Christopher Daniel Yépez Chandi <75264703+ChristopherYepezch@users.noreply.github.com>
2023-10-20 11:34:37 -0500

    Update app.js
    Signed-off-by: Christopher Daniel Yépez Chandi <75264703+ChristopherYepezch@users.noreply.github.com>

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('*', (req, res) => {
     res.redirect('/');
 })
 
+app.get('/myhome'(req,res) => {
+    res.status(200).send({message:"Funcionando"});
+})
+
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
